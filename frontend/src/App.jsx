1d188e4b3b39573475bc2666fd3c54fdad2722cc
himanshusssingh <himanshusingh39397@gmail.com>
2025-08-27 23:18:04 +0530

    Components structures

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import ChatWindow from './ChatWindow.jsx'
+import Sidebar from './Sidebar.jsx'
+
+function App() {
+  return (
+    <div className='app'>
+    <Sidebar></Sidebar>
+    <ChatWindow></ChatWindow>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Sidebar.css'
+
+function Sidebar (){
+    return(
+ <section className="sidebar">
+            <button>
+                <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo"></img>
+                <span><i className="fa-regular fa-pen-to-square"></i></span>
+            </button>
+
+
+            <ul className="history">
+                <li>Threads</li>
+                <li>Threads</li>
+                <li>Threads</li>
+                <li>Threads</li>
+                <li>Threads</li>
+            </ul>
+ 
+            <div className="sign">
+                <p>By Himanshu Singh &hearts;</p>
+            </div>
+        </section>
+    )
+}
+
+export default Sidebar
